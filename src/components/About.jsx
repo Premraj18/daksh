5c524e5586216df5fbd514db1cc27bda63745cef
Premraj18 <premrajprasad18@gmail.com>
2024-01-06 02:52:08 +0530

    abc

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -79,18 +79,18 @@ const About = () => {
                     <div className="flex flex-wrap ">
                         <div className="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
                             <div className="relative lg:max-w-md">
-                                <img src="https://i.postimg.cc/rF0MKfBV/pexels-andrea-piacquadio-3760263.jpg" alt="aboutimage"
+                                <img src="/about.jpg" alt="aboutimage"
                                     className="relative z-10 object-cover w-full rounded h-96" />
                                 <div
-                                    className="absolute bottom-0 right-0 z-10 p-8 bg-white border-4 border-lime-600 rounded shadow dark:border-lime-500 lg:-mb-8 lg:-mr-11 sm:p-8  " style={{ backgroundColor: mode === 'dark' ? '#3b3f47' : '', color: mode === 'dark' ? 'lightgray' : '', }}>
-                                    <p className="text-lg font-semibold md:w-72" >
+                                    className="absolute bottom-0 right-0 z-10 p-6 bg-white border-4 border-lime-600 rounded shadow dark:border-lime-500 lg:-mb-8 lg:-mr-11 sm:p-8  " style={{ backgroundColor: mode === 'dark' ? '#3b3f47' : '', color: mode === 'dark' ? 'lightgray' : '', }}>
+                                    <p className="text-2xl font-semibold md:w-72" >
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                                             className="absolute top-0 left-0 w-16 h-16 text-lime-600 dark:text-gray-300 opacity-10"
                                             viewBox="0 0 16 16">
                                             <path
                                                 d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z">
                                             </path>
-                                        </svg> Successfully Providing business solutions from 25 years
+                                        </svg> If you can dream it, you can do it.
                                     </p>
                                 </div>
                             </div>
