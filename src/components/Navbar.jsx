36b4556faaf9a68862e35ce424f462b57b5480e9
Premraj18 <premrajprasad18@gmail.com>
2024-01-06 18:08:20 +0530

    ABC

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -125,7 +125,7 @@ export default function Navbar() {
                     HOME
                   </Link>
                   <Link to={'/about'} className="text-sm font-medium text-gray-700 hover:text-lime-600 hover:scale-110" style={{ color: mode === 'dark' ? 'white' : '', }}>
-                    Our Participation
+                    OUR PARTICIPATION
                   </Link>
                   <Link to={'/achivements'} className="text-sm font-medium text-gray-700 hover:text-lime-600 hover:scale-110" style={{ color: mode === 'dark' ? 'white' : '', }}>
                     ACHIEVEMENTS
