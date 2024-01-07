e2949560c187a0267c626c34a48e9fbe7958a904
Premraj18 <premrajprasad18@gmail.com>
2024-01-07 12:23:43 +0530

    issue fix

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,19 +1,22 @@
 import React, { useContext, useState } from 'react'
 import Layout from '../../components/Layout'
 import myContext from '../../context/MyContext'
-import manufacturing from './manufacturing.json'
+import Manufacturing from './manufacturing.json'
 import Workshop from './Workshop.json'
 import events from './events.json'
 import GroupPhoto from './GroupPhoto.json'
-import celebration from './celebration.json'
+import Celebration from './Celebration.json'
 import GalleryCard from './GalleryCard'
 
 const Gallery = () => {
-    const [item, setItem] = useState(manufacturing)
-    const event = () => {
-        let item2 = events
-        setItem(item2)
-    }
+    
+    const [active, setActive] = useState("item1")
+    const [item1, setItem1] = useState(Manufacturing)
+    const [item2, setItem2] = useState(events)
+    const [item3, setItem3] = useState(Workshop)
+    const [item4, setItem4] = useState(GroupPhoto)
+    const [item5, setItem5] = useState(Celebration)
+
     const context = useContext(myContext)
     const { toggleMode, mode } = context
     return (
@@ -26,20 +29,24 @@ const Gallery = () => {
 
                     <div className='flex justify-center flex-wrap w-full mb-48'>
                         <button className="sm:w-36 w-40 mx-5 my-2 px-4 py-2 text-gray-100 bg-lime-600 rounded dark:bg-lime-500 dark:hover:bg-lime-700 hover:bg-lime-700"
-                            onClick={() => setItem(manufacturing)}>Manufacturing</button>
+                            onClick={() => setActive('item1')}>Manufacturing</button>
                         <button className="sm:w-36 w-40 mx-5 my-2 px-4 py-2 text-gray-100 bg-lime-600 rounded dark:bg-lime-500 dark:hover:bg-lime-700 hover:bg-lime-700"
-                            onClick={() => event()}>Events</button>
+                            onClick={() => setActive('item2')}>Events</button>
                         <button className="sm:w-36 w-40 mx-5 my-2 px-4 py-2 text-gray-100 bg-lime-600 rounded dark:bg-lime-500 dark:hover:bg-lime-700 hover:bg-lime-700"
-                            onClick={() => setItem(Workshop)}>Workshop</button>
+                            onClick={() => setActive('item3')}>Workshop</button>
                         <button className="sm:w-36 w-40 mx-5 my-2 px-4 py-2 text-gray-100 bg-lime-600 rounded dark:bg-lime-500 dark:hover:bg-lime-700 hover:bg-lime-700"
-                            onClick={() => setItem(GroupPhoto)}>Group Photos</button>
+                            onClick={() => setActive('item4')}>Group Photos</button>
                         <button className="sm:w-36 w-40 mx-5 my-2 px-4 py-2 text-gray-100 bg-lime-600 rounded dark:bg-lime-500 dark:hover:bg-lime-700 hover:bg-lime-700"
-                            onClick={() => setItem(celebration)}>Celebration</button>
+                            onClick={() => setActive('item5')}>Celebration</button>
                        
                     </div>
 
                     <div className='flex justify-center md:gap-x-24 gap-y-20 flex-wrap' style={{ marginTop: '-80px' }} >
-                        <GalleryCard item={item} />
+                        {active === 'item1' && <GalleryCard item={item1} />}
+                        {active === 'item2' && <GalleryCard item={item2} />}
+                        {active === 'item3' && <GalleryCard item={item3} />}
+                        {active === 'item4' && <GalleryCard item={item4} />}
+                        {active === 'item5' && <GalleryCard item={item5} />}
 
                     </div>
 
